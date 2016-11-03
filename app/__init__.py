396fc03ec18fc195797bc642bce666aa76392f7e
Fabiano <famube@gmail.com>
2016-11-03 15:18:38 -0200

    Database model updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 mantag = Flask(__name__)
+mantag.config.from_object('config')
+db = SQLAlchemy(mantag)
 
-from app import views
+from app import views, models
+
+
--- app/models.py
+++ app/models.py
@@ -2,36 +2,73 @@ from app import db
 
 
 class User(db.Model):
-    #__tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    nickname = db.Column(db.String)
     
         
     def __init__(self, id, nickname):
         self.id = id
-        self.nickname = nickname
+        self.nickname = name
         
     def __repr__(self):
-        return '<User %r ID:%d>' % (self.name, self.id)
+        return '<User %r ID:%d>' % (self.nickname, self.id)
 
 
-#Association table
-#obj_tags = db.Table('obj_tags', db.metadata,
-#    db.Column('obj_id', db.String, db.ForeignKey('objects.id')),
-#    db.Column('tag', db.String, db.ForeignKey('tags.string'))
-#)
+
+
+#Object-Tag Relationship Table
+obj_tags = db.Table('obj_tags',
+    db.Column('obj_id', db.String, db.ForeignKey('object.id')),
+    db.Column('tag', db.String, db.ForeignKey('tag.string'))
+)
+
 
 class Object(db.Model):
-    #__tablename__ = 'objects'
     id = db.Column(db.String, primary_key=True)
     title = db.Column(db.String)
     
+    # 0 = regular object
+    # 1 = only trivial relevant tags
+    #-1 = only trivial irrelevant tags
+    sanity_test =  db.Column(db.Integer)
+    
     #many to many Object <=> Tag
-    #tags = db.relationship("Tag", secondary=obj_tags)
+    
+    tags = db.relationship('Tag', secondary=obj_tags,
+        backref=db.backref('objects', lazy='dynamic'))
 
-    #tags = db.relationship('Tag', backref='tag_obj', lazy='dynamic')
 
-#class Tag(db.Model):
-    #__tablename__ = 'tags'
-#    string = db.Column(db.String, primary_key=True)
+class Tag(db.Model):
+    string = db.Column(db.String, primary_key=True)
+    
+
+
+class Judgement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    eval_id = db.Column('eval_id', db.Integer, db.ForeignKey('evaluation.id'))
+    tag = db.Column('tag', db.String, db.ForeignKey('tag.string'))
+    
+    #relevance label
+    relevant = db.Column('label', db.Boolean)   
+
+
+#judgements = db.Table('judgements',
+#    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#    db.Column('obj_id', db.String, db.ForeignKey('object.id')),
+#    db.Column('tag', db.String, db.ForeignKey('tag.string')),
+#    db.Column('label', db.Boolean),   
+#)
+
+
+class Evaluation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    obj_id = db.Column('obj_id', db.String, db.ForeignKey('object.id'))
+    previous_knowledge = db.Column(db.Integer)
+    timestamp = db.Column('timestamp', db.DateTime)
+   
+    judgements = db.relationship('Judgement', backref='evaluation', lazy='dynamic')
+
+
+
+
