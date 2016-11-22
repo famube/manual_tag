82053408c7eab1de27a4fdc51dcccaa110a49cf3
Fabiano <famube@gmail.com>
2016-11-22 14:03:57 -0200

    Evaluation form included, but not finished

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,14 @@ from wtforms.validators import DataRequired, EqualTo, Length, NumberRange
 from flask_wtf.html5 import IntegerField
 
 class RegisterForm(Form):
-    age = IntegerField('Age') 
+    age = IntegerField('Age', validators=[DataRequired()]) 
     name = TextField('Name', validators=[Length(min=4, max=120)])
     gender = RadioField('Gender', choices=[('M', 'Masculino'),('F', 'Feminino')])
+    
+
+class EvaluationForm(Form):        
+    __init__(self, obj):
+       self.prev_knowledge = RadioField('Knowledge', choices=[('Muito', 'Muito'), ('Pouco', 'Pouco'), ('Nada', 'Nada')])
+       self.evaluation_tags = SelectField('Tags')
+       self.evaluation_tags.choices = [(tag.string, tag.string) for tag in obj.tags]
+
--- app/views.py
+++ app/views.py
@@ -1,34 +1,52 @@
 from flask import render_template, flash, session, url_for, redirect, request, g
 from app import mantag, db
-from .forms import RegisterForm
-from .models import User
+from .forms import RegisterForm, EvaluationForm
+from .models import User, Object, Evaluation
+from random import shuffle
 
-@mantag.route('/', methods = ['GET', 'POST'])
+@mantag.route('/index', methods = ['GET', 'POST'])
 def index():
     form = RegisterForm()
     #return render_template('index.html', form=form)
     if form.validate_on_submit:
+        if 'user_id' in session:
+            print("session_userid=", session['user_id'])
+#            session.pop('user_id')
+        if form.name.data == None:
+            return render_template('index.html', form=form)
         user = User(form.name.data, form.age.data, form.gender.data)
         db.session.add(user)
-        db.session.commit
+        db.session.commit()
         flash('We can start now')
         session['user_id'] = user.id
+        print("Registered user:", user)
         return redirect(url_for('evaluate'))
     return render_template('index.html', form=form)
 
 
-@mantag.route('/evaluate')
+@mantag.route('/evaluate', methods=['GET', 'POST'])
 def evaluate():
-    if "id" in session:
-        current_id = session['id']
-        #evaluated = models.Evaluation.query.filter_by(user_id=current_id)
-        #evaluated_ids = set([evaluation.obj_id for evaluation in evaluated])
+    if 'user_id' in session:
+        current_id = session['user_id']
+        print("session_userid=", current_id)
+        
+        
+        #Objects already evaluated by the current user
+        
+        evaluated = Evaluation.query.filter_by(user_id=current_id)
+        evaluated_ids = set([evaluation.obj_id for evaluation in evaluated])
         
         #random select the objs to be avaluated, compare the objects returned by the query
         #with the one already evaluated by the user
         
-        obj = models.Object.query.get(obj_id)
-
-        return render_template('evaluate.html', obj=obj)
+        objs = Object.query.all()
+        
+        for obj in shuffle(objs):
+            if obj.id not in evaluated_ids:
+                form = EvaluationForm(obj)
+                return render_template('evaluate.html', obj=obj, form=form)
     else:
         return redirect(url_for('index'))
+
+
+
